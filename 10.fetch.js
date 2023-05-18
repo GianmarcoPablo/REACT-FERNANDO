41ff52cd7c3a74a9bc49ec7995c485d0a494292f
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-05-17 23:37:17 -0500

    first commit

--- 10.fetch.js
+++ 10.fetch.js
@@ -1,12 +1,12 @@
-const apiKey = `R3o1gEZM3OS8grP2BGGLA9XckTWzhm1v`;
+// const apiKey = `R3o1gEZM3OS8grP2BGGLA9XckTWzhm1v`;
 
-const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+// const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-peticion.then(respuesta=>respuesta.json())
-        .then(({data})=>{
-            const {url} = data.images.original
-            const img = document.createElement("img")
-            img.src = url
-            document.body.append(img)
-        })
-        .catch(console.warn);
+// peticion.then(respuesta=>respuesta.json())
+//         .then(({data})=>{
+//             const {url} = data.images.original
+//             const img = document.createElement("img")
+//             img.src = url
+//             document.body.append(img)
+//         })
+//         .catch(console.warn);
